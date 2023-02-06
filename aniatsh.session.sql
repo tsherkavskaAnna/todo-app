7ef2d47d1bce1f41ec69e474c1017258beef38e7
Ania <80473872+tsherkavskaAnna@users.noreply.github.com>
2023-02-06 23:08:28 +0100

    connect with postgres

---
+++ aniatsh.session.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE todoapp;
+
+CREATE TABLE todos (
+    id VARCHAR(255) PRIMARY KEY,
+    user_email VARCHAR(255),
+    title VARCHAR(255),
+    progress INT,
+    date VARCHAR(300)
+);
+
--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ const express = require('express')
 const app = express()
 const pool = require('./db')
 
+console.log(pool);
+app.get('/', (req, res) => {
+  res.json(pool)
+})
 //get all todos//
 app.get('/todos', async (req, res) => {
+  
     try {
       const todos = await pool.query('SELECT * FROM todos')
-      res.json(todos.rows)
+      res.json(todos)
     } catch (error) {
         console.error(error);  
     }
 })
+
 
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
